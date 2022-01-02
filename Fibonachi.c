c217f00ae3396a396e28f39fc6ff249e31168771
brandon0331 <79501630+brandon0331@users.noreply.github.com>
2022-01-02 23:59:16 +0900

    Create Fibonachi.c

---
+++ Fibonachi.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+	int a, b, c = 0;
+	int n;
+	
+	scanf("%d", &n);
+
+	a = 0;
+	b = 1;
+
+	for (int i = 0; i < n; i++) {
+		a = b;
+		b = c;
+		c = a + b;
+	}
+	printf("%d", c);
+}
